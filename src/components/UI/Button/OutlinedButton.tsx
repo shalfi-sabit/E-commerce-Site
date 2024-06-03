11fa243243784c6b7ce9a6be7c21dbecbabda900
Chowdhury Nazmuz Sakib <nazmuz.sakib@brainstation-23.com>
2024-06-03 13:01:06 +0600

    Create Reusable Section Header

--- src/components/UI/Button/OutlinedButton.tsx
+++ src/components/UI/Button/OutlinedButton.tsx
@@ -1,5 +1,5 @@
-interface Props{
-    text:string;
+interface Props {
+    text: string;
 }
 
 
@@ -9,7 +9,7 @@ const OutlinedButton:React.FC<Props> = ({text}) => {
             <button className="border-solid border-[1px] rounded border-slate-500 text-black 
         font-semibold hover:text-slate-700 text-[12px] sm:text-sm lg:text-[16px] px-5 sm:px-6 lg:px-7 py-1 sm:py-[6px] lg:py-2 hover:shadow-md duration-200 ">
             {text}
-        </button>
+            </button>
         </div>
     );
 };
--- src/components/UI/SectionHeader.tsx
+++ src/components/UI/SectionHeader.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-
-const SectionHeader = () => {
+interface Props{
+    sectionHeader: string;
+}
+const SectionHeader:React.FC<Props>= ({sectionHeader}) => {
     return (
-        <div>
-            
-        </div>
+        <header className="font-bold sm:text-base text-sm text-red-500">
+            <span className="bg-red-500 rounded mr-2 sm:mr-4 py-[5px] sm:py-[7px] px-[7px] sm:px-[9px]" /> {sectionHeader}
+        </header>
+
     );
 };
 
