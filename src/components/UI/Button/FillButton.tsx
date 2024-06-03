afcefce76774f9066ec4d48bc40581e75d41e2ad
Ibrahim Ratul <ibrahim.ratul@brainstation-23.com>
2024-06-03 15:04:58 +0600

    remove button-fill border

--- src/components/UI/Button/FillButton.tsx
+++ src/components/UI/Button/FillButton.tsx
@@ -8,8 +8,8 @@ const Button: React.FC<ButtonProps> = ({ text }) => {
   return (
     <div>
       <button
-        className="bg-red-900 hover:bg-red-400 border-solid border-[1px] rounded border-slate-500 text-white 
-    font-semibold hover:text-slate-700 text-[12px] sm:text-sm lg:text-[16px] px-5 sm:px-6 lg:px-7 py-1 sm:py-[6px] lg:py-2 hover:shadow-md duration-200 "
+        className="bg-red-900 hover:bg-red-400 rounded text-white 
+    font-semibold text-[12px] sm:text-sm lg:text-[16px] px-5 sm:px-6 lg:px-7 py-1 sm:py-[6px] lg:py-2 hover:shadow-md duration-200"
       >
         {text}
       </button>
