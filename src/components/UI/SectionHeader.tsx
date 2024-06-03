91e1d35a66100cdea908f357e0d97d724c415cc4
Chowdhury Nazmuz Sakib <nazmuz.sakib@brainstation-23.com>
2024-06-03 15:27:50 +0600

    change the color according to tailwind config file

--- src/components/UI/SectionHeader.tsx
+++ src/components/UI/SectionHeader.tsx
@@ -3,8 +3,8 @@ interface Props{
 }
 const SectionHeader:React.FC<Props>= ({sectionHeader}) => {
     return (
-        <header className="font-bold sm:text-base text-sm text-red-500">
-            <span className="bg-red-500 rounded mr-2 sm:mr-4 py-[5px] sm:py-[7px] px-[7px] sm:px-[9px]" /> {sectionHeader}
+        <header className="font-bold sm:text-base text-sm text-red-900">
+            <span className="bg-red-900 rounded mr-2 sm:mr-4 py-[5px] sm:py-[7px] px-[7px] sm:px-[9px]" /> {sectionHeader}
         </header>
 
     );
