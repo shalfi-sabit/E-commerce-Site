f7eefb144aa59be8344653259784e5d140196bca
Ibrahim Ratul <ibrahim.ratul@brainstation-23.com>
2024-06-05 20:01:09 +0600

    preemptively fix an issue to avoid merge conflict

--- src/components/pages/RootLayout/index.tsx
+++ src/components/pages/RootLayout/index.tsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom";
 
 import Navbar from "../../shared/Navbar";
-import BreadCrumb from "../../shared/BreadCrumb";
 
 const RootLayout = () => {
   return (
     <main>
       <Navbar />
-      <BreadCrumb />
       <Outlet></Outlet>
     </main>
   );
