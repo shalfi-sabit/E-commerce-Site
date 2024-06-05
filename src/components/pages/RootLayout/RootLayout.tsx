2339d3f32163f0ac64ce4797222ee22fe0bc82a9
Ibrahim Ratul <ibrahim.ratul@brainstation-23.com>
2024-06-05 13:08:16 +0600

    remove unnecessary code from RootLayout.tsx

--- src/components/pages/RootLayout/RootLayout.tsx
+++ src/components/pages/RootLayout/RootLayout.tsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom";
 
 import Navbar from "../../shared/Navbar/Navbar";
-import CustomerBenefits from "../../shared/CustomerBenefits";
 
 const RootLayout = () => {
   return (
     <main>
       <Navbar />
-      <CustomerBenefits />
       <Outlet></Outlet>
     </main>
   );
