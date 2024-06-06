e44f437dd63a71e5eeb977e6518b1455c6b44552
Chowdhury Nazmuz Sakib <nazmuz.sakib@brainstation-23.com>
2024-06-06 15:37:07 +0600

    Add react-hook form and yup functionality for form control

--- src/components/UI/Input/FormInput.tsx
+++ src/components/UI/Input/FormInput.tsx
@@ -1,19 +1,31 @@
 import { useOnFocus } from "../../../hooks/useOnFocus";
+import { MdErrorOutline } from "react-icons/md";
+
 interface FormInputProps {
+  className: string;
   label?: string;
   placeholder?: string;
+  type?: string;
+  name: string;
   required?: boolean;
+  register: any;
+  errors?: any;
 }
 
 const FormInput: React.FC<FormInputProps> = ({
+  className,
   label,
   placeholder,
+  type,
   required,
+  name,
+  register,
+  errors,
 }) => {
   const { isFocused, handleFocus, handleBlur } = useOnFocus();
 
   return (
-    <div className="flex flex-col gap-1 pb-3 md:pb-4 xl:pb-5 w-2/4">
+    <div className={`flex flex-col gap-1 pb-3 md:pb-4 xl:pb-5 ${className}`}>
       {label && (
         <label
           className={`${
@@ -24,12 +36,27 @@ const FormInput: React.FC<FormInputProps> = ({
         </label>
       )}
       <input
-        className="bg-gray-100 rounded focus:outline-none placeholder-gray-800 focus:text-black-900 text-[10px] md:text-[12px] lg:text-sm p-2 md:p-3"
+        className={`bg-gray-100 rounded focus:outline-none placeholder-gray-800 focus:text-black-900 text-[10px] md:text-[12px] lg:text-sm p-2 md:p-3 ${
+          errors[name]?.message ? "" : "mb-8"
+        }`}
         required={required}
         placeholder={placeholder}
+        type={type}
+        {...register(name)}
         onFocus={handleFocus}
         onBlur={handleBlur}
       />
+      {errors[name]?.message ? (
+        <p
+          className={`text-red-900 tracking-tighter text-[10px] md:text-[12px] lg:text-sm mt-1 flex items-center gap-1 ${
+            errors[name]?.message ? "mb-5" : ""
+          }`}
+        >
+          <MdErrorOutline /> {errors[name]?.message}
+        </p>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
--- src/components/pages/RootLayout/index.tsx
+++ src/components/pages/RootLayout/index.tsx
@@ -1,6 +1,5 @@
-import { Outlet } from "react-router-dom";
-
 import Navbar from "../../shared/Navbar";
+import { Outlet } from "react-router-dom";
 import Footer from "../../shared/Footer";
 
 const RootLayout = () => {
