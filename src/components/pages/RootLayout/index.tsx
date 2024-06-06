c536688c9ba8adeb859d8f7c79d77249acf4aeb6
Salfi Sabit <shalfi.sabit@brainstation23.com>
2024-06-06 21:13:56 +0600

    setup redux store and complete snackbar component

--- src/components/pages/RootLayout/index.tsx
+++ src/components/pages/RootLayout/index.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 
 import Navbar from "../../shared/Navbar";
+import Snackbar from "../../Snackbar";
 
 const RootLayout = () => {
   return (
     <main>
       <Navbar />
+      <Snackbar />
 
       <Outlet></Outlet>
     </main>
