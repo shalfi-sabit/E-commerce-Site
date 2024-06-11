9be3355645842b0c883caf843c3ec760b1f7047b
Chowdhury Nazmuz Sakib <nazmuz.sakib@brainstation-23.com>
2024-06-11 21:02:31 +0600

    Complete top rated products component

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,8 +1,10 @@
 import FlashSale from "../../FlashSales";
+import TopRatedProducts from "../../TopRatedProducts";
 const Home = () => {
   return (
     <main className="overflow-x-hidden">
       <FlashSale />
+      <TopRatedProducts />
     </main>
   );
 };
